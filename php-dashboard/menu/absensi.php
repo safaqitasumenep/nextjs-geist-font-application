a8c5801228442de2ed124ed8f84b7a1e4e16f6f3
safaqitasumenep <sumenep.safaqita@gmail.com>
2025-06-19 18:40:33 +0700

    Add php-dashboard/menu/absensi.php 1750333233253

---
+++ php-dashboard/menu/absensi.php
@@ -0,0 +1,7 @@
+<?php
+// Halaman dummy Monitoring Absensi
+?>
+<div class="container mt-3">
+    <h2>Monitoring Absensi</h2>
+    <p>Ini adalah halaman dummy untuk menu Monitoring Absensi.</p>
+</div>
