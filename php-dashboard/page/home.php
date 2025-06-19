26ade024e347d307b3d96bc2c9db438d7c52e156
safaqitasumenep <sumenep.safaqita@gmail.com>
2025-06-19 18:40:37 +0700

    Add php-dashboard/page/home.php 1750333238164

---
+++ php-dashboard/page/home.php
@@ -0,0 +1,7 @@
+<?php
+// Halaman home default untuk admin.php?id=home
+?>
+<div class="container mt-3">
+    <h2>Selamat datang di Dashboard Admin</h2>
+    <p>Ini adalah halaman utama dashboard admin.</p>
+</div>
