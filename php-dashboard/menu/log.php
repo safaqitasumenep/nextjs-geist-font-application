4b7f91ddb4c2ff79210c7f6709ce0352333903e2
safaqitasumenep <sumenep.safaqita@gmail.com>
2025-06-19 18:40:26 +0700

    Add php-dashboard/menu/log.php 1750333226966

---
+++ php-dashboard/menu/log.php
@@ -0,0 +1,7 @@
+<?php
+// Halaman dummy History & Log Saldo
+?>
+<div class="container mt-3">
+    <h2>History & Log Saldo</h2>
+    <p>Ini adalah halaman dummy untuk menu History & Log Saldo.</p>
+</div>
